036aa4ed99fe63f4ff327f866d041de67dd2b04e
bonorb <bogdan.norbert@liveedu.mathiasz.hu>
2025-11-11 11:34:25 +0100

    laravel database, migration
    elkeszultek a migraciok

---
+++ BBSbackend/database/migrations/2025_11_11_101859_create_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("user");
+            $table->string("password");
+            $table->string("name");
+            $table->integer("phone");
+            $table->boolean("loyal");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
