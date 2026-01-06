5bd03b6fc371b1c829e6471aff59d3b10b74ed25
bonorb <bogdan.norbert@liveedu.mathiasz.hu>
2026-01-06 14:09:55 +0100

    login panel
    a login pa

--- BBSfrontend/src/App.jsx
+++ BBSfrontend/src/App.jsx
@@ -1,15 +1,20 @@
 import UserLayout from "./components/UserLayout";
 import AdminLayout from "./components/AdminLayout";
+import LoginComponent from "./components/LoginComponent.jsx";
 import "./App.css";
 import HomePageComps from "./components/HomePageComps";
 import { Person } from "react-bootstrap-icons";
 function App() {
   return (
     <>
+<<<<<<< Updated upstream
       <UserLayout name="Példáné Kovács Marika">
         <HomePageComps key={1} icon={Person} value="teszt" title="valami"></HomePageComps>
       </UserLayout>
       
+=======
+      <LoginComponent />
+>>>>>>> Stashed changes
     </>
   );
 }
